ad89c269810d6f6b5f35cfce32196c9cc4302da2
Dowon Kim <dowoniscool@yahoo.com>
2024-09-03 20:42:19 +0200

    initial commit with foldering the original porject shape

---
+++ NetworkingStudy/App/NetworkingStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingStudyApp.swift
+//  NetworkingStudy
+//
+//  Created by Dowon Kim on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NetworkingStudy/Core/AllCoins/New Group/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkingStudy
+//
+//  Created by Dowon Kim on 03/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
