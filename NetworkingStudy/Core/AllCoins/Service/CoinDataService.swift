33682ce3ba7c3c34facd3ade44fbdf3be781b385
Dowon Kim <dowoniscool@yahoo.com>
2024-10-03 08:49:27 +0200

    Async/Await | Swift Concurrency - Networking with Async/Await

--- NetworkingStudy/Core/AllCoins/Service/CoinDataService.swift
+++ NetworkingStudy/Core/AllCoins/Service/CoinDataService.swift
@@ -5,6 +5,24 @@ class CoinDataService {
     //Delete "i" below for a test from ""https://api.coingeck"
     private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h&locale=en"
     
+    func fetchCoins() async throws -> [Coin] {
+        guard let url = URL(string: urlString) else { return [] }
+        
+        print("DEBUG: Fetching data..")
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let coins = try JSONDecoder().decode([Coin].self, from: data)
+            return coins
+        } catch {
+            print("DEBUG: Error \(error.localizedDescription)")
+            return []
+        }
+    }
+}
+
+// MARK: - Completion Handlers
+
+extension CoinDataService {
     func fetchCoinsWithResult(completion: @escaping(Result<[Coin], CoinAPIError>) -> Void) {
         guard let url = URL(string: urlString) else { return }
         
@@ -38,27 +56,7 @@ class CoinDataService {
             }
         }.resume()
     }
-    
-    
-    func fetchCoins(completion: @escaping([Coin]?, Error?) -> Void) {
-        guard let url = URL(string: urlString) else { return }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let data = data else { return }
-            
-            guard let coins = try? JSONDecoder().decode([Coin].self, from: data) else {
-                print("DEBUG: Failed to decode coins")
-                return }
-            
-            completion(coins, nil)
-        }.resume()
-    }
-    
+
     func fetchPrice(coin: String, completion: @escaping(Double) -> Void) {
         let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return }
@@ -67,17 +65,17 @@ class CoinDataService {
             
                 if let error = error {
                     print("DEBUG: Failed with error \(error.localizedDescription)")
-//                    self.errorMessage = error.localizedDescription
+    //                    self.errorMessage = error.localizedDescription
                     return
                 }
                 
                 guard let httpResponse = response as? HTTPURLResponse else {
-//                    self.errorMessage = "Bad HTTP Response"
+    //                    self.errorMessage = "Bad HTTP Response"
                     return
                 }
                 
                 guard httpResponse.statusCode == 200 else {
-//                    self.errorMessage = "Failed to fetch with status code \(httpResponse.statusCode)"
+    //                    self.errorMessage = "Failed to fetch with status code \(httpResponse.statusCode)"
                     return
                 }
                                 
@@ -88,11 +86,12 @@ class CoinDataService {
                 }
                 guard let price = value["usd"] else { return }
                 
-//                self.coin = coin.capitalized
-//                self.price = "$\(price)"
+    //                self.coin = coin.capitalized
+    //                self.price = "$\(price)"
                 print("DEBUG: Price in service is \(price)")
                 completion(price)
             
         }.resume()
     }
+
 }
--- NetworkingStudy/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingStudy/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -8,21 +8,14 @@ class CoinsViewModel: ObservableObject {
     private let service = CoinDataService()
     
     init() {
-        fetchCoins()
+        Task { try await fetchCoins() }
     }
     
-    func fetchCoins() {
-//        service.fetchCoins { coins, error in
-//                DispatchQueue.main.async {
-//                    if let error = error {
-//                        self.errorMessage = error.localizedDescription
-//                        return
-//                    }
-//                    
-//                    self.coins = coins ?? []
-//                }
-//            }
-        //-> without using Result type
+    func fetchCoins() async throws {
+        self.coins = try await service.fetchCoins()
+    }
+    
+    func fetchCoinsWithCompletionHandler() {
         service.fetchCoinsWithResult { [weak self] result in
             DispatchQueue.main.async {
                 switch result {
