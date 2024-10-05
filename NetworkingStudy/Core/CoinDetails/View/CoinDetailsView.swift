54316b8fdc153296bf693133147e494348854263
Dowon Kim <dowoniscool@yahoo.com>
2024-10-05 23:50:50 +0200

    Async/Await | Swift Concurrency - Task Modifier Add Task Modifier to resolve some performance issue like Calling API 2 times unnecessary and if user exit from detail pages quick enough before loading then API task will be cancelled

--- NetworkingStudy/Core/CoinDetails/View/CoinDetailsView.swift
+++ NetworkingStudy/Core/CoinDetails/View/CoinDetailsView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct CoinDetailsView: View {
     let coin: Coin
     @ObservedObject var viewModel: CoinDetailsViewModel
+//    @State private var task: Task<(), Never>?
+    // Ugly way to Task and Cancel 1/2 -> uncomment this and the code below as the example
     
     init(coin: Coin) {
         self.coin = coin
@@ -17,21 +19,36 @@ struct CoinDetailsView: View {
     }
     
     var body: some View {
-        if let details = viewModel.coinDetails {
+        
             VStack(alignment: .leading) {
-                Text(details.name)
-                    .fontWeight(.semibold)
-                    .font(.subheadline)
-                
-                Text(details.symbol.uppercased())
-                    .font(.footnote)
-                
-                Text(details.description.text)
-                    .font(.footnote)
-                    .padding(.vertical)
+                if let details = viewModel.coinDetails {
+                    Text(details.name)
+                        .fontWeight(.semibold)
+                        .font(.subheadline)
+                    
+                    Text(details.symbol.uppercased())
+                        .font(.footnote)
+                    
+                    Text(details.description.text)
+                        .font(.footnote)
+                        .padding(.vertical)
+                }
             }
+            .task {
+                await viewModel.fetchCoinDetails()
+            }
+            // -> much much clear way to do it
+        
+//            .onAppear {
+//                self.task = Task { await viewModel.fetchCoinDetails() }
+//            }
+//            .onDisappear {
+//                task?.cancel()
+//            }
+        // Ugly way to Task and Cancel 2/2 -> uncomment this and the code above as the example
+
             .padding()
-        }
+        
     }
 }
 
--- NetworkingStudy/Core/CoinDetails/ViewModel/CoinDetailsViewModel.swift
+++ NetworkingStudy/Core/CoinDetails/ViewModel/CoinDetailsViewModel.swift
@@ -15,13 +15,16 @@ class CoinDetailsViewModel: ObservableObject {
     @Published var coinDetails: CoinDetails?
     
     init(coinId: String) {
+        print("DEBUG : Did CoinDetailsViewModel Init?")
         self.coinId = coinId
-        
-        Task { await fetchCoinDetails() }
     }
     
     @MainActor
     func fetchCoinDetails() async {
+        print("DEBUG: Fetching Coins")
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        print("DEBUG: Task woke up")
+        
         do {
             let details = try await service.fetchCoinDetails(id: coinId)
             print("DEBUG: Details \(details)")
