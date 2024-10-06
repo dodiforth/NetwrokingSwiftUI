156bcb196c669a3e4e43c77a8926d521a343d966
Dowon Kim <dowoniscool@yahoo.com>
2024-10-06 16:00:52 +0200

    Refactor: Move HTTPDataDownloader protocol and extension to Shared/API folder

--- NetworkingStudy/Core/AllCoins/Service/CoinDataService.swift
+++ NetworkingStudy/Core/AllCoins/Service/CoinDataService.swift
@@ -1,35 +1,5 @@
 import Foundation
 
-protocol HTTPDataDownloader {
-    func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T
-}
-
-extension HTTPDataDownloader {
-    func fetchData<T: Decodable>(as type: T.Type, endpoint: String) async throws -> T {
-        
-        guard let url = URL(string: endpoint) else {
-            throw CoinAPIError.requestFailed(description: "Invalid URL")
-        }
-        
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw CoinAPIError.requestFailed(description: "Request failed")
-        }
-        
-        guard httpResponse.statusCode == 200 else {
-            throw CoinAPIError.invalidStatusCode(statusCode: httpResponse.statusCode)
-        }
-                
-        do {
-            return try JSONDecoder().decode(type, from: data)
-        } catch {
-            print("DEBUG: Error \(error)")
-            throw error as? CoinAPIError ?? .unknownError(error: error)
-        }
-    }
-}
-
 class CoinDataService: HTTPDataDownloader {
     
     //Delete "i" below for a test from ""https://api.coingeck"
