b599ed0e151405d97d7cb2bac4f849a73af8de2a
Dowon Kim <dowoniscool@yahoo.com>
2024-10-13 19:22:01 +0200

    🔨 Unit Testing

---
+++ NetworkingStudyTests/TestData.swift
@@ -0,0 +1,14 @@
+//
+//  TestData.swift
+//  NetworkingStudyTests
+//
+//  Created by Dowon Kim on 13/10/2024.
+//
+
+import Foundation
+
+let testCoinsData: Data = """
+
+
+
+""".data(using: .utf8)!
