e8f6769c9b33c86b8a62decd104c1d1a87d9a09a
Dowon Kim <dowoniscool@yahoo.com>
2024-10-18 23:55:03 +0200

    🔨Unit Testing : Testing The Coins View Model

--- NetworkingStudyTests/TestData.swift
+++ NetworkingStudyTests/TestData.swift
@@ -284,3 +284,66 @@ let mockCoinData_marketCapDesc: Data = """
 ]
 
 """.data(using: .utf8)!
+
+// Invalid name key in bitcoin
+let mockCoins_invalidJSON = """
+
+[
+  {
+    "id": "bitcoin",
+    "symbol": "btc",
+    "nasdnfjlkasdjdf;'lasjfl;ksaj;": "Bitcoin",
+    "image": "https://assets.coingecko.com/coins/images/1/large/bitcoin.png",
+    "current_price": 28500,
+    "market_cap": 542000000000,
+    "market_cap_rank": 1,
+    "fully_diluted_valuation": 600000000000,
+    "total_volume": 24000000000,
+    "high_24h": 28700,
+    "low_24h": 28250,
+    "price_change_24h": -200,
+    "price_change_percentage_24h": -0.70,
+    "market_cap_change_24h": -4000000000,
+    "market_cap_change_percentage_24h": -0.73,
+    "circulating_supply": 19000000,
+    "total_supply": 21000000,
+    "max_supply": 21000000,
+    "ath": 69000,
+    "ath_change_percentage": -58.70,
+    "ath_date": "2021-11-10T00:00:00.000Z",
+    "atl": 67,
+    "atl_change_percentage": 42500,
+    "atl_date": "2013-07-05T00:00:00.000Z",
+    "last_updated": "2024-10-14T23:47:36.000Z"
+  },
+  {
+    "id": "ethereum",
+    "symbol": "eth",
+    "name": "Ethereum",
+    "image": "https://assets.coingecko.com/coins/images/279/large/ethereum.png",
+    "current_price": 1750,
+    "market_cap": 210000000000,
+    "market_cap_rank": 2,
+    "fully_diluted_valuation": 210000000000,
+    "total_volume": 12000000000,
+    "high_24h": 1780,
+    "low_24h": 1725,
+    "price_change_24h": -30,
+    "price_change_percentage_24h": -1.68,
+    "market_cap_change_24h": -3500000000,
+    "market_cap_change_percentage_24h": -1.65,
+    "circulating_supply": 120000000,
+    "total_supply": 120000000,
+    "max_supply": null,
+    "ath": 4890,
+    "ath_change_percentage": -64.20,
+    "ath_date": "2021-11-16T00:00:00.000Z",
+    "atl": 0.432979,
+    "atl_change_percentage": 404612,
+    "atl_date": "2015-10-20T00:00:00.000Z",
+    "last_updated": "2024-10-14T23:47:36.000Z"
+  }
+]
+
+
+""".data(using: .utf8)!
